8517a443e8aa742d08ef646ad9a44358367317d8
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-05-14 20:30:49 -0300

    feat: add initial Terraform configuration for EKS and VPC setup
    - Create .gitignore to exclude unnecessary files
    - Define EKS cluster and node group resources
    - Configure AWS provider and backend settings
    - Set up VPC with public and private subnets, route tables, and NAT gateway
    - Implement security group rules for web access
    - Add output variables for VPC and subnets
    - Include necessary variables for environment and subnet configurations

---
+++ eks/node-groups.tf
@@ -0,0 +1,26 @@
+# To use node groups :
+# Private Kubernetes node group with EC2 Instances
+resource "aws_eks_node_group" "private_nodes" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "private-nodes"
+  node_role_arn   = "arn:aws:iam::775370709632:role/LabRole"
+
+  subnet_ids = module.vpc.private_subnets
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t2.small"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 6
+    min_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "private-nodes"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  token      = var.aws_session_token
+}
+
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+
+module "eks" {
+  source       = "./eks"
+  cluster_name = "eks-tech-challenge"
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_session_token" {}
+variable "aws_region" {}
