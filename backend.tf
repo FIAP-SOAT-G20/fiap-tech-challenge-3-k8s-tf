ff76e4724625ed277345802e5ba321f167dd37ee
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2025-05-24 06:16:53 -0500

    refactor(backend): update S3 key path for Terraform state

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ff-tech-challenge-eks-cluster-backend-tf"
-    key = "fiap/terraform.tfstate"
+    key = "fiap/k8s/terraform.tfstat"
     region = "us-east-1"
   }
 }
