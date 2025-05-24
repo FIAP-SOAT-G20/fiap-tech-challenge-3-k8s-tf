9f03878862721e4177709c9e2aa4cb78933109d7
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-05-24 09:55:42 -0300

    chore(outputs): remove deprecated API Gateway outputs from Terraform configuration

--- outputs.tf
+++ outputs.tf
@@ -26,26 +26,10 @@ output "subnet_ids" {
   value = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.region}e"]
 }
 
-# output "alb_dns" {
-#   value = aws_alb.alb.dns_name
-# }
-
 output "lab_role_arn" {
   value = var.lab_role
 }
 
 output "principal_arn" {
   value = var.principal_arn
-}
-
-output "api_gateway_url" {
-  value = "https://${aws_api_gateway_rest_api.fast_food_api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}"
-}
-
-output "api_gateway_id" {
-  value = aws_api_gateway_rest_api.fast_food_api.id
-}
-
-output "api_gateway_root_resource_id" {
-  value = aws_api_gateway_rest_api.fast_food_api.root_resource_id
 }
