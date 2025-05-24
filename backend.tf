1afb20d6c5b5cee3cfb5f43576b782bfd3cc65fb
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-05-24 09:30:51 -0300

    fix(backend): correct typo in Terraform state key path

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ff-tech-challenge-eks-cluster-backend-tf"
-    key = "fiap/k8s/terraform.tfstat"
+    key = "fiap/k8s/terraform.tfstate"
     region = "us-east-1"
   }
 }
