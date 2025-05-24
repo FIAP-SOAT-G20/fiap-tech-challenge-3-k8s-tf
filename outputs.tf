b114718582345a1024ff6601762032420144d1a4
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2025-05-24 06:09:15 -0500

    feat(outputs): add API Gateway ID and root resource ID outputs

--- outputs.tf
+++ outputs.tf
@@ -41,3 +41,11 @@ output "principal_arn" {
 output "api_gateway_url" {
   value = "https://${aws_api_gateway_rest_api.fast_food_api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}"
 }
+
+output "api_gateway_id" {
+  value = aws_api_gateway_rest_api.fast_food_api.id
+}
+
+output "api_gateway_root_resource_id" {
+  value = aws_api_gateway_rest_api.fast_food_api.root_resource_id
+}
